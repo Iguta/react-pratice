0a2b6e7f556ecd3da7b9b616f6377182548d884b
Iguta <davidiguta.5028@gmail.com>
2021-12-17 21:35:35 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-txzd2n
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-txzd2n)
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect} from 'react';
+import ReactDOM from 'react-dom';
+
+function App(props) {
+  const [counter, setCounter] = useState(0);
+  function increment() {
+    console.log('I have been clicked...');
+    setCounter(counter + 1);
+  };
+  //useEffect -> for side effect functions of the component
+  // -> Runs when the component is rendered, and every time the component is updated
+  useEffect (() => {
+    alert(`You have clicked ${coutimes`)
+  }, [counter])
+  return (
+    <div>
+      <div>
+        <h1>Hello {props.name}</h1>
+        <p>Start editing to see some magic happen :)</p>
+      </div>
+      <div>
+        <h3>You have clicked {counter} times </h3>
+        <button onClick={increment}>Click me</button>
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(<App name={'David'} />, document.getElementById('root'));
